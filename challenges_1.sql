3947b4de59fcc9729c20b7da77cac230c927977c
Hashini Liyanage <33922245+hashinil@users.noreply.github.com>
2024-02-21 12:42:31 +0100

    Update challenges_1.sql

--- challenges_1.sql
+++ challenges_1.sql
@@ -7,6 +7,11 @@ SELECT first_name, last_name, email
 FROM customer;
 
 -- Challenge 2
+-- order the customer list that you have already given by the last name.
+-- And they want to start working from Z towards a indebt list.
+-- And also in case there is the same last name multiple times in the list, you should as a second criterion
+-- also order by the first name and the first name in this case, as a second criterion should also be from Z to A.
+
 SELECT first_name, last_name, email  
 FROM customer
 ORDER BY last_name DESC, first_name DESC;
@@ -16,22 +21,28 @@ FROM customer
 ORDER BY 2 DESC, 1 DESC;
 
 -- Challenge 3
+-- different prices in the business that have been paid in the past 
+-- also order those prices from high to low.
 SELECT DISTINCT amount
 FROM payment
 ORDER BY amount DESC
 LIMIT 10;
 
 -- challenge 4
+-- create a list of all of the distinct districts
 SELECT DISTINCT(district)
 FROM address;
 
+-- what is the latest rental date in the database?
 SELECT rental_date 
 FROM rental 
 ORDER By rental_date DESC
 LIMIT 1;
 
+-- how many films does the company have?
 SELECT COUNT(*)
 FROM film;
 
+-- how many distinct last names of the customers are there in our database?
 SELECT COUNT(DISTINCT last_name) 
 FROM customer;
